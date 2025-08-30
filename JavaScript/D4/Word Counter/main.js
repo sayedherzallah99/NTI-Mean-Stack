80844f0a3264ad812f420e05ba1bbf425db08776
Sayed Herzallah <your-email@example.com>
2025-08-30 11:58:29 +0300

    JavaScript

--- JavaScript/D4/Word Counter/main.js
+++
@@ -1,6 +0,0 @@
-var sentence = window.prompt("Enter a sentence:");
-let words = sentence.trim().split(" ");
-console.log("Total words:", words.length);
-words.forEach(word => {
-  console.log(word + " => " + word.length + " letters");
-});
